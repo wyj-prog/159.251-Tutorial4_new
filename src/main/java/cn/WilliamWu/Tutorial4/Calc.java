9d25165e252ace42447322824d066ce3f47da769
wyjgit <1078075534@qq.com>
2022-09-27 11:37:31 +0800

    delete one dependency again

--- src/main/java/cn/WilliamWu/Tutorial4/Calc.java
+++ src/main/java/cn/WilliamWu/Tutorial4/Calc.java
@@ -1,2 +1,10 @@
-package cn.WilliamWu.Tutorial4;public class Calc {
+package cn.WilliamWu.Tutorial4;
+
+public class Calc {
+    public static int add(int a, int b){
+        return a + b;
+    }
+//    public static int subtract(int a, int b){
+//        return a + b;
+//    }
 }
--- src/test/java/cn/WilliamWu/Tutorial4/TestCalc.java
+++ src/test/java/cn/WilliamWu/Tutorial4/TestCalc.java
@@ -1,2 +1,16 @@
-package cn.WilliamWu.Tutorial4;public class TestCalc {
+package cn.WilliamWu.Tutorial4;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestCalc {
+    @Test
+    public void Testadd(){
+        assertEquals(2, cn.WilliamWu.Tutorial4.Calc.add(1,1));
+    }
+
+//    @Test
+//    public void TestSubtract(){
+//
+//    }
 }
